2ae03931d48d601e5d2ab5d8bd2246f134787168
bryanbilek <bbilek35@gmail.com>
2020-01-31 16:04:21 -0600

    finished sprint

--- challenges/classes.js
+++ challenges/classes.js
@@ -13,7 +13,7 @@ class CuboidMaker {
         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
     }
 };
-let cuboid = new CuboidMaker({
+let cuboid2 = new CuboidMaker({
     length: 4,
     width: 5,
     height: 5,
@@ -21,7 +21,7 @@ let cuboid = new CuboidMaker({
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid2.volume()); // 100
+console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -100,6 +100,11 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes('Uni')) {
+    unisWithUni.push(graduates[i]);
+  }
+}
 console.log(unisWithUni);
 
 
@@ -126,6 +131,10 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function (element) {
+  const names = `${element.animal_name}, ${element.scientific_name}`;
+  displayNames.push(names)
+})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -134,7 +143,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(name => name.animal_name.toLowerCase());
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -143,6 +152,11 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+for (let i = 0; i < zooAnimals.length; i++) {
+  if (zooAnimals[i].population < 5) {
+    lowPopulationAnimals.push(zooAnimals[i])
+  }
+}
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -150,9 +164,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue.population;
+}, 0);
 console.log(populationTotal);
-
 
 /*
 
